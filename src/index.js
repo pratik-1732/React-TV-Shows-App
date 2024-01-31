30bfc03c61c0d5aea202a6742ae17c34401490c8
Pratik <patilpratikp035@gmail.com>
2024-01-31 19:05:23 +0530

    updating css file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './App.css'; // Import your CSS file
 import App from './App';
+import './App.css';  // Correct import statement for App.css
 
 ReactDOM.render(
   <React.StrictMode>
