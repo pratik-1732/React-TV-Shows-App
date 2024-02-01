1b358424cc25847988e53b5ac621170b6d6194a3
Pratik <patilpratikp035@gmail.com>
2024-02-01 19:25:26 +0530

    html and js file updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './App.css';  // Correct import statement for App.css
+// In index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
